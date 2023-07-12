d3c51962d18e8dcf84d7e53401753ed5c577b407
Ivan Tutunaru <itutunaru1998@gmail.com>
2023-07-12 21:31:34 +0300

    Polymorphism
    - Made game manager a singleton.
    - Added a public method SetText to update the current display text.
    - Extended Shape class to detect mouse clicks and update the current display text.
    - Added DisplayText overrides for Cube, Cylinder and Sphere classes.

--- Assets/Scripts/Shape.cs
+++ Assets/Scripts/Shape.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Shape : MonoBehaviour
 {
+    protected virtual void DisplayText() {
+        GameManager.Instance.SetText("Shape clicked!");
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,20 @@ public class Shape : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        CheckForClick();
+    }
+
+    void CheckForClick() {
+        if (!Input.GetMouseButtonDown(0)) return;
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (!Physics.Raycast(ray, out RaycastHit hit) || hit.transform == null || hit.transform.gameObject != gameObject) return;
+
+        OnClick();
+    }
+
+    void OnClick() {
+        DisplayText();
     }
 }
